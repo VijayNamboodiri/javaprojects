7d2642d1e003489fca7b826e08e10e254fbd23a1
VijayNamboodiri <85519890+VijayNamboodiri@users.noreply.github.com>
2021-10-08 18:21:54 -0700

    Add files via upload

---
+++ jokenorms/src/geniusnorms.java
@@ -0,0 +1,23 @@
+
+public class geniusnorms {
+
+	public static void main(String[] args) {
+		System.out.println("Group Members: Quincy, Vijay, Jonathon");
+		System.out.println("About Quincy");
+		System.out.println("Quincy likes video games, especially rogue likes. He cooks often for his family, and He's pretty good at math.");
+		System.out.println("About Vijay");
+		System.out.println("I love to play video games,and my favorite food is pasta"
+				+ " I joined the hidden genius project because I wanted to learn more about coding and buisness, and empower myself and others."
+				+ " I go to Bentley School in lafaeyette"); 
+		System.out.println("About Jonathon");
+		System.out.println("Jonathon likes to draw, play video games and read. He loves to write short stories and eventually wants to make his own web comic");
+		System.out.println("Here are some of our community norms");
+		System.out.println("--------------------------------------");
+		System.out.println("Communicate with Everyone, "
+				+ "Ask Brothers for Help,  "
+				+ "Help Others,  "
+				+ "Never Give Up, Keep Going!, "
+				+ "And Finally, No Cussing ");
+	}
+
+}

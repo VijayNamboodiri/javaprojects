5b0379ed43ae21907f22ceb631a57a0b6259dc5f
VijayNamboodiri <85519890+VijayNamboodiri@users.noreply.github.com>
2021-10-27 13:15:13 -0700

    Add files via upload

---
+++ Genius.java
@@ -0,0 +1,69 @@
+package myProjects;
+
+public class Genius {
+	public static void main(String[] args) {
+		System.out.println("Test");
+		gotNames();
+		gotJokes();
+		gotCommunityNorms();
+	}
+	
+	/*gotNames Method Declaration */
+	 public static void gotNames() {
+			 
+			 /* GENIUS NAMES - SAY OUR NAMES */
+			 System.out.println("My Name Is Vijay Namboodiri");  //Print Genius Name
+			 System.out.println("My Name Is Jonathon Manion");  //Print Genius Name
+			 System.out.println("My Name Is Quincy Braxton");  //Print Genius Name
+			 System.out.println("");  //Extra Line
+	        	 System.out.println("");  //Extra Line
+			 
+		 }
+	 /*gotJokes Method Declaration */
+	 public static void gotJokes() {
+		 
+		 /* GENIUS JOKES - WE GOT JOKES */	
+       System.out.println("Welcome, My Name Is Vijay!");  //Print My Name
+       System.out.println("What's a skeletons favorite instrument");  //Joke Setup 
+       System.out.println("The Xylobone");  //Joke Punch Line
+       System.out.println("");  //Extra Line
+       System.out.println("");  //Extra Line
+		 
+       
+	 }
+	 /*gotCommunityNorms Method Declaration */
+	 public static void gotCommunityNorms() {
+		/* GENIUS COMMUNITY NORMS - KEEP US ACCOUNTABLE */
+		System.out.println("OUR COMMUNITY NORMS");
+		System.out.println("----------------------");
+		System.out.println("1. No Cussing");
+		System.out.println("2. Communicate with everyone");
+		System.out.println("3. Ask Brothers for Help");
+		System.out.println("4. Be respectful");
+		System.out.println("5. Communicate with instructors");
+		System.out.println("----------------------");
+		System.out.println("");  //Extra Line
+       System.out.println("");  //Extra Line
+			 
+	 
+	 }
+	 /* gotAthletics Method Declaration */ 
+	 public static void gotAthletics() {
+		 System.out.println("Jonathon Plays Soccer");
+		 System.out.println("Quincy Plays Basketball");
+		 System.out.println("Vijay Plays Baseball");
+
+	 }
+	 public static void gotDrawing() {
+		 System.out.println("Jonathon has been drawing for 6 years");
+		 System.out.println("Vijay has been drawing since the start of quarantine ");
+		 System.out.println("Quincy also enjoys drwaing, and does it on and off");
+	 }
+	 public static void gamingAdaptability() {
+		 System.out.println("When faced with a challenge while gaming: Vijay tries to get a second a opinion, and then goes back into it");
+		 System.out.println("When faced with a challenge while gaming: Jonathon tries new strategies untill he finally overcomes the challenge");
+		 System.out.println("When faced with a challenge while gaming: Quincy tries and if he fails he learns from that mistake");
+	 }
+}
+
+

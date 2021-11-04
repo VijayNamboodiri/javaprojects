6bb3c6e4a6c68e46e1259de56a68567caa6cc6d3
VijayNamboodiri <85519890+VijayNamboodiri@users.noreply.github.com>
2021-11-03 19:33:07 -0700

    Add files via upload

---
+++ sweetProgram.java
@@ -0,0 +1,21 @@
+package myProjects;
+
+import java.util.Scanner;
+
+public class sweetProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("What is your name?");
+		
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello " + name);
+		
+	}
+
+	 {
+	}
+	
+
+}
